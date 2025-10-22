f94ba8dfa01485e112bb461350aebc71f2fed9ae
Kalana Methsara <kalanamethsara53@gmail.com>
2025-10-23 01:23:33 +0530

    clear filds

--- assets/js/loging.js
+++ assets/js/loging.js
@@ -4,33 +4,39 @@ const loginFormContainer = document.querySelector(".login-form-container");
 const registerBtn = document.querySelector(".register-btn");
 const loginBtn = document.querySelector(".login-btn");
 
+const loginForm = document.querySelector(".form-box.login form");
+const registerForm = document.querySelector(".form-box.register form");
+const dashboard = document.getElementById("dashboard");
+const logoutBtn = document.getElementById("logout-btn");
+
 registerBtn.addEventListener("click", () => {
   loginFormContainer.classList.add("active");
+  loginForm.reset();
+  registerForm.reset();
+  loginPassword.type = "password";
+  showLoginPassword.checked = false;
 });
 
 loginBtn.addEventListener("click", () => {
   loginFormContainer.classList.remove("active");
+  loginForm.reset();
+  registerForm.reset();
+  loginPassword.type = "password";
+  showLoginPassword.checked = false;
 });
 
 const loginPassword = document.getElementById("login-password");
 const showLoginPassword = document.getElementById("show-login-password");
-
 showLoginPassword.addEventListener("change", function () {
   loginPassword.type = this.checked ? "text" : "password";
 });
 
 const registerPassword = document.getElementById("register-password");
 const showRegisterPassword = document.getElementById("show-register-password");
-
 showRegisterPassword.addEventListener("change", function () {
   registerPassword.type = this.checked ? "text" : "password";
 });
 
-
-const loginForm = document.querySelector(".form-box.login form");
-const registerForm = document.querySelector(".form-box.register form");
-const dashboard = document.getElementById("dashboard");
-const logoutBtn = document.getElementById("logout-btn");
 
 loginForm.addEventListener("submit", function (e) {
   e.preventDefault();
@@ -46,6 +52,7 @@ loginForm.addEventListener("submit", function (e) {
     alert("✅ Login successful!");
     loginFormContainer.style.display = "none";
     dashboard.style.display = "block";
+    loginForm.reset();
   } else {
     alert("❌ Invalid username or password!");
   }
@@ -66,6 +73,9 @@ registerForm.addEventListener("submit", function (e) {
   user_array.push({ username, password });
   alert("✅ Registered successfully! You can now login.");
 
+  registerForm.reset();
+  registerPassword.type = "password";
+  showRegisterPassword.checked = false;
   loginFormContainer.classList.remove("active");
 });
 
@@ -73,4 +83,8 @@ logoutBtn.addEventListener("click", function () {
   dashboard.style.display = "none";
   loginFormContainer.style.display = "flex";
   loginFormContainer.classList.remove("active");
+  loginForm.reset();
+  registerForm.reset();
+  loginPassword.type = "password";
+  showLoginPassword.checked = false;
 });
